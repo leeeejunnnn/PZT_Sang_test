92aa345d9abb8a6f484c057c8d014528df85b3b0
leeeejunnnn <shins2aya@gmail.com>
2021-05-04 15:02:52 +0900

    ㅔㅑ

---
+++ data_pipeline.py
@@ -0,0 +1,47 @@
+#%%
+import os
+from glob import glob
+
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms, utils
+from torch.utils.data import Dataset, DataLoader
+
+import matplotlib.pyplot as plt
+
+# %%
+data_dir = './dataset/'
+
+#%%
+class data_pipeline(Dataset):
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
+        self.num_data = []
+        self.num_target = []
+        self.data = []
+        self.target = []
+        data_list = sorted(glob(self.data_dir + '*time*.npy'))
+        target_list = sorted(glob(self.data_dir + '*ref*.npy'))
+        for i in range(len(data_list)):
+            data_a = np.load(data_list[i])
+            data_a = data_a.reshape(-1,1200)
+            target_a = np.load(target_list[i])
+            target_a = target_a.reshape(-1,1)
+            self.num_data.append(data_a.shape[0])
+            self.num_target.append(target_a.shape[0])
+            self.data.append(data_a)
+            self.target.append(target_a)
+        self.data = np.vstack(self.data)
+        self.target = np.vstack(self.target)
+
+    def __len__(self):
+        return (self.data.shape[0])
+
+    def __getitem__(self, idx):
+        target = self.target[idx,:]
+        x = self.data[idx,:]
+        return x, target
+
+
