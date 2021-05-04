9d97cd7bc3f75f579232d5dfa163f1e0943d6f55
leeeejunnnn <shins2aya@gmail.com>
2021-05-04 17:44:50 +0900

    filemerge

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # PZT_Sang_test
+
+file_merge.py: intact / damaged 각 폴더에서 개별신호를 하나의 파일로 취합. 
+
---
+++ file_merge.py
@@ -0,0 +1,56 @@
+#%%
+import os
+from glob import glob
+
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import transforms, utils
+from torch.utils.data import Dataset, DataLoader
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from natsort import natsorted
+
+# %%
+data_dir = './dataset'
+
+intact_list = sorted(glob(data_dir+'/Intact/*')) 
+damaged_list = sorted(glob(data_dir + '/Damaged/*'))
+print(len(intact_list), len(damaged_list))
+# %%
+for i in intact_list:
+    dataset = glob(i)
+    print(dataset)
+    dataset_list = natsorted(glob(dataset[0] + '/*'))
+    #print(dataset_list)
+    file_name = dataset
+    new_dataset = []
+    for j in dataset_list:
+        read_data = pd.read_csv(j, index_col=0, sep='\t',header=None, encoding='utf-8')
+        new_dataset.append(read_data.values.tolist())
+    data=np.vstack(new_dataset)
+    res_data=np.reshape(data,(33,-1))
+    print(res_data.shape)
+    np.save(data_dir+'/'+dataset[0].split('/')[-1]+'.npy', res_data)
+
+print('done')
+# %%
+for i in damaged_list:
+    dataset = glob(i)
+    print(dataset)
+    dataset_list = natsorted(glob(dataset[0] + '/*'))
+    #print(dataset_list)
+    file_name = dataset
+    new_dataset = []
+    for j in dataset_list:
+        read_data = pd.read_csv(j, index_col=0, sep='\t',header=None, encoding='utf-8')
+        new_dataset.append(read_data.values.tolist())
+    data=np.vstack(new_dataset)
+    res_data=np.reshape(data,(33,-1))
+    print(res_data.shape)
+    np.save(data_dir+'/'+dataset[0].split('/')[-1]+'.npy', res_data)
+
+print('done')
